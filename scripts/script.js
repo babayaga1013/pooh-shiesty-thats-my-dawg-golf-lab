431ddec939d1ec13d3195ad443fdd5fcb57e7e3e
babayaga1013 <157440894+babayaga1013@users.noreply.github.com>
2024-03-19 23:42:29 -0500

    Delete scripts/script.js

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("pgMenu");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
