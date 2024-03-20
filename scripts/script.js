c4383b995c9ddeaa349fec2e029e9a42a9ef6bdd
babayaga1013 <157440894+babayaga1013@users.noreply.github.com>
2024-03-20 11:49:59 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -12,47 +12,55 @@ function toggleMenu() {
 
     const games = [
       {
-        title: "007",
-        author: "James Bond", 
-        link: "Games Bond"
-      
+        title: "super-adventure-charlie",
+        author: "Skyler Saunders", 
+        link: "https://skysaunders.github.io/super-adventure-charlie/",
+        
+        title2: "Name Game",
+        author2: "Skyler Saunders", 
+        link2: "https://skysaunders.github.io/animated-sniffle-foxtrot/"
       },
       {
-        title: "Name Game",
-        link: "https://zanemoon13.github.io/lab-echo/index.html", 
-        author: "Zane Mooney"
+        title: "Glowing Meme Foxtrot",
+        author: "Candice Buford", 
+        link: "https://cbuf0rd.github.io/glowing-meme-foxtrot/",
+        
+        title2: "Glowing Giggle Echo Lab",
+        author2: "Candice Buford", 
+        link2: "https://cbuf0rd.github.io/glowing-giggle-echo-lab/"
       },
-      
-        {
-          title: "namePage",
-          link: "https://2fortdev.github.io/Echo-lab/",
-          author: "Sam Aaron"
-          }
+      {
+        // this is for the last game notice it doesnt have a comma on the closed curly bracket 
+      }
     ];
     // iterate over the JSON array
     $.each(games, function (index, item) {
       console.log(item.title);
       console.log(item.author);
       console.log(item.link);
+      console.log(item.title2);
+      console.log(item.author2);
+      console.log(item.link2);
     
     
 
     var el = `<div class='container'> \
       <div class='card'> \
            <h3>${ item.title } </h3>\
-           <h4>${ item.author } </h4> \
+           <h4>By ${ item.author } </h4> \
            <a href="${ item.link }" target="_blank" class="link-info" >Game 🎮</a> \
+       </div>
+           <div class='card'> \
+           <h3>${ item.title2 } </h3>\
+           <h4>By ${ item.author2 } </h4> \
+           <a href="${ item.link2 }" target="_blank" class="link-info" >Game 🎮</a> \
        </div>
        </div>`;
      
     
      console.log(el); 
     
-    
-    $('#firstCard').append(el);
-    
+    $('#gameCard').append(el);
     
     });
-    
-    
     });
